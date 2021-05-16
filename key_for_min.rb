d147d3d9f635258faa0354b8c1c92cbdb9054a08
Heshie Brody <4hbrody@gmail.com>
2021-05-16 03:30:03 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,11 +5,14 @@ name_hash = {:chair => 25, :table => 85, :mattress => 450}
 
 def key_for_min_value(name_hash)
   min = nil
+  min_key = nil
   name_hash.each do |key, value|
     if min == nil
-      min = key
+      min_key = key
+      min = value
     elsif value < min
       min = value
+      min_key = key
     else
       puts("nope")
     end
